a867766894d404b10aac0ef488b54b203b19de8e
Mika Nokka <mika.nokka@ambientia.fi>
2021-06-02 12:20:36 +0300

    Changes permission scheme to listed project, but gives errors when executed.

--- ChangePermissions.groovy
+++ ChangePermissions.groovy
@@ -7,6 +7,7 @@ import org.apache.log4j.Logger
 import org.apache.log4j.Level
 import com.atlassian.jira.user.ApplicationUser
 import com.atlassian.jira.permission.PermissionSchemeService
+import com.atlassian.jira.permission.PermissionScheme
 import com.atlassian.jira.permission.PermissionSchemeManager
 
 
@@ -14,28 +15,34 @@ import com.atlassian.jira.permission.PermissionSchemeManager
 def projectManager = ComponentAccessor.getProjectManager()
 def pss = ComponentAccessor.getComponent(PermissionSchemeService)
 
+
+// ************** CONFIGURATIONS ACCORDING THE JIRA **************
+// list of project to be having new permission scheme
+def thelist=["Test Permission 1","Test Permission Second Project","Test Permissions Task Project"]
+// Test permission scheme ID fromU I
+def permmissionid=10201
+// ************** END OF CONFIGURATIONS **************
+
 Logger mylogger = Logger.getLogger("permissions")
 mylogger.setLevel(Level.DEBUG)
-mylogger.info("---------- LOG_SLA_TIMES started ------------------------------")
+mylogger.info("---------- permissions started ------------------------------")
 
 
 ApplicationUser loggedinuser=ComponentAccessor.getJiraAuthenticationContext().getLoggedInUser()
 mylogger.debug("loggedinuser:$loggedinuser")
-def iduser = loggedinuser.getKey()
-mylogger.debug("iduser:$iduser")
-def projects = projectManager.getProjectObjects()
 
-def listed=[]
+
 def id
-def permscheme
-projects.each {
-	id = it.getId()
-	mylogger.debug("$it.name   id:$id")
-	permscheme=pss.getSchemeAssignedToProject( loggedinuser, id)
-	mylogger.debug("permscheme:$permscheme")
-	
-	listed.add(it.name)
+def projobject
+
+thelist.each {
+	projobject = projectManager.getProjectObjByName(it)
+	id=projobject.getId()
+	mylogger.debug("Project: $it id:$id")
+	// might be usefull to do the validation checking first in case of problems
+	pss.assignPermissionSchemeToProject(loggedinuser, permmissionid, id)
 }
 
-mylogger.debug("${listed}")
-mylogger.info("---------- LOG_SLA_TIMES ended ------------------------------")
+
+
+mylogger.info("---------- permissions ended ------------------------------")
